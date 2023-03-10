eccdff4f22f3eabd49c743f552f224e50ff5d532
Alvaro Sacari <alvaro.sacari@gmail.com>
2023-03-09 23:16:22 -0500

    feat: add buttons to change the language

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,13 @@
         <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
         <v-toolbar-title>{{ $t('todoApp.title') }}</v-toolbar-title>
         <v-spacer></v-spacer>
-        <v-btn icon>
+        <v-btn icon @click="$i18n.locale = 'es'" :outlined="$i18n.locale === 'es'">
+          <v-avatar>ES</v-avatar>
+        </v-btn>
+        <v-btn class="ml-1" icon @click="$i18n.locale = 'en'" :outlined="$i18n.locale === 'en'">
+          <v-avatar>EN</v-avatar>
+        </v-btn>
+        <v-btn icon class="ml-1">
           <v-icon>mdi-magnify</v-icon>
         </v-btn>
       </v-app-bar>
